79a608d2c2baf070d0dc41b0644c460ef18be7f5
yvcdev <95775140+yvcdev@users.noreply.github.com>
2022-02-04 23:06:40 -0500

    routing fixed

--- lib/routes/route_converter.dart
+++ lib/routes/route_converter.dart
@@ -1,10 +1,5 @@
 import 'package:t_helper/activity_screens/c_sort_sentence_screen.dart';
-import 'package:t_helper/screens/screens.dart';
-import 'package:t_helper/widgets/home_wrapper.dart';
 
 final routeConverter = {
   "sort-the-sentence": const CSortSentenceScreen(),
-  "/HomeWrapper": const HomeWrapper(),
-  "/RegisteredGroupScreen": const RegisteredGroupScreen(),
-  "/LoginScreen": const LoginScreen(),
 };
